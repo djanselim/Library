7638a8f75a25071b6da54ee95e4de7eaf8401ede
djanselim <114089140+djanselim@users.noreply.github.com>
2024-03-24 12:47:04 +0200

    Add project files.

---
+++ Library/Data/Models/IdentityUserBook.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Data.Models
+{
+	[Comment("User Books")]
+	public class IdentityUserBook
+	{
+		[Comment("Book Collector")]
+		public string CollectorId { get; set; } = null!;
+
+		[Comment("Collector")]
+		public IdentityUser IdentityUser { get; set; } = null!;
+
+		[Comment("Book Id")]
+		public int BookId { get; set; }
+
+		[Comment("Book")]
+		public Book Book { get; set; } = null!;
+	}
+}
