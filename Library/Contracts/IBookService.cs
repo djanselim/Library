7c32e3551928a5bc0db227474965fd685e7697e7
djanselim <114089140+djanselim@users.noreply.github.com>
2024-03-26 23:01:51 +0200

    Interface created

---
+++ Library/Contracts/IBookService.cs
@@ -0,0 +1,12 @@
+﻿using Library.Models;
+
+namespace Library.Contracts
+{
+	public interface IBookService
+	{
+		Task AddBookToUserCollectionAsync(string userId, BookViewModel book);
+		Task<IEnumerable<AllBookViewModel>> GetAllBooksAsync();
+		Task<BookViewModel?> GetBookByIdAsync(int id);
+		Task<IEnumerable<AllBookViewModel>> GetMyBooksAsync(string userId);
+	}
+}
